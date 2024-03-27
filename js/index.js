2d746c25d59b6dac5c17a59d042910533b32a77d
Richard <mbouandagnanga18@gmail.com>
2024-03-27 14:50:49 +0000

    Update the color

--- js/index.js
+++ js/index.js
@@ -3,6 +3,6 @@ document.addEventListener("DOMContentLoaded", function () {
   let changeColorBtn = document.getElementById("change-color-btn");
   colorBox.addEventListener("click", function () {
     this.style.backgroundColor =
-      "#" + Math.floor(Math.random() * 16777215).toString(16);
+      "#" + Math.floor(Math.random() * 0123456789).toString(16);
   });
 });
